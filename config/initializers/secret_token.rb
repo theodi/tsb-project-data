5131d4385eb8895d672f4b100e4952cd895a2db2
RicSwirrl <ric@swirrl.com>
2013-09-14 12:08:55 +0100

    first commit - completely vanilla rails 3.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TsbProjectData::Application.config.secret_token = 'ac8d61cd4164360d9a6283fb7b28f2bd054cc650f9a2b97d12af1803c784b1e8fb527c9a9420b43f9448974cc261a9999349dfece6ac1ae2668f00e7369e687f'

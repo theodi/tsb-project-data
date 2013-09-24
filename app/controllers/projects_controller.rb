f99344c62b47d8c06dc5b25ecfaf191321cf2465
RicSwirrl <ric@swirrl.com>
2013-09-24 17:18:08 +0100

    error pages

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,8 +2,9 @@ class ProjectsController < ApplicationController
 
   # list of projects
   def index
-    criteria = Project.all
 
+    # TODO: change to use faceted search.
+    criteria = Project.all
     @pagination_params = PublishMyData::ResourcePaginationParams.from_request(request)
     @projects = PublishMyData::Paginator.new(criteria, @pagination_params).paginate
   end

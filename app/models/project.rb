924453cc873e2b2b6e1e85a5944f540bbddb6216
RicSwirrl <ric@swirrl.com>
2013-09-26 19:43:25 +0100

    rearrange some stuff on the project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,38 @@
 class Project
 
+  # RDF-related
+  ##############
+
   include TsbResource
+
+  graph_uri TsbProjectData::DATA_GRAPH
+  rdf_type Vocabulary::TSBDEF.Project
+
+  # literals
+  field :label, RDF::RDFS.label
+  field :description, Vocabulary::DCTERMS.description
+  field :project_number, Vocabulary::TSBDEF.projectNumber
+  field :start_date, Vocabulary::TSBDEF.startDate
+  field :end_date, Vocabulary::TSBDEF.endDate
+
+  # links
+  linked_to :leader, Vocabulary::TSBDEF.hasLeader, class_name: 'Organization'
+  linked_to :supported_by, Vocabulary::TSBDEF.supportedBy, class_name: 'Grant'
+
+  def self.uri_from_slug(slug)
+    "http://#{PublishMyData.local_domain}/id/#{slug}"
+  end
+
+  def dataset
+    PublishMyData::Dataset.find(PublishMyData::Dataset.uri_from_data_graph_uri(self.graph_uri)) rescue nil
+  end
+
+   # Elastic Search
+  #################
 
   include Tire::Model::Search
 
-  tire.mapping do
+   tire.mapping do
     indexes :name, :type => 'string', :analyzer => 'snowball'
     indexes :leader_name, :type => 'string', :analyzer => 'snowball'
   end
@@ -31,26 +59,5 @@ class Project
   # http://www.elasticsearch.org/guide/reference/api/search/facets/
   # http://www.elasticsearch.org/guide/reference/api/search/facets/terms-stats-facet/
 
-  graph_uri TsbProjectData::DATA_GRAPH
-  rdf_type Vocabulary::TSBDEF.Project
-
-  # literals
-  field :label, RDF::RDFS.label
-  field :description, Vocabulary::DCTERMS.description
-  field :project_number, Vocabulary::TSBDEF.projectNumber
-  field :start_date, Vocabulary::TSBDEF.startDate
-  field :end_date, Vocabulary::TSBDEF.endDate
-
-  # links
-  linked_to :leader, Vocabulary::TSBDEF.hasLeader, class_name: 'Organization'
-  linked_to :supported_by, Vocabulary::TSBDEF.supportedBy, class_name: 'Grant'
-
-  def self.uri_from_slug(slug)
-    "http://#{PublishMyData.local_domain}/id/#{slug}"
-  end
-
-  def dataset
-    PublishMyData::Dataset.find(PublishMyData::Dataset.uri_from_data_graph_uri(self.graph_uri)) rescue nil
-  end
 
 end

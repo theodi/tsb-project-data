fdff51976c9e44fb3647f04c0902bfb300db08a0
RicSwirrl <ric@swirrl.com>
2013-09-29 10:16:55 +0100

    added some more fields to the concept concern

--- app/models/concerns/concept.rb
+++ app/models/concerns/concept.rb
@@ -11,6 +11,10 @@ module Concept
     field :label, RDF::RDFS.label
     field :description, Vocabulary::DCTERMS.description
 
+    field :sub_class_of, RDF::SKOS.subClassOf, :is_uri => true
+    field :broader, RDF::SKOS.broader, :is_uri => true
+    field :narrower, RDF::SKOS.narrower, :is_uri => true, :multivalued => true
+
     rdf_type RDF::SKOS.Concept
 
     def initialize(uri, graph_uri=nil)

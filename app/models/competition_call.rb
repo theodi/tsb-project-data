85a1bf3eaf9a4f12c712b5394d17b0c4abeb2b66
RicSwirrl <ric@swirrl.com>
2013-09-29 21:26:32 +0100

    remove comment

--- app/models/competition_call.rb
+++ app/models/competition_call.rb
@@ -1,13 +1,12 @@
 class CompetitionCall
 
-  # Question: should this be a resource in the dataset, or a concept in a scheme?
   include TsbResource
 
   rdf_type Vocabulary::TSBDEF.CompetitionCall
-  
+
   field :competition_code, Vocabulary::TSBDEF.competitionCode
   field :competition_year, Vocabulary::TSBDEF.competitionYear, is_uri: true
-  
+
   # TODO: update predicates
   linked_to :team, Vocabulary::TSBDEF.team
   linked_to :budget_area,  Vocabulary::TSBDEF.budgetArea

b4f1df5ba5c38d8a166b0b5cd508564cbeec7a9c
Bill <bill@swirrl.com>
2013-10-01 18:14:35 +0200

    enterprise size concept scheme

--- lib/import/enterprise_sizes.rb
+++ lib/import/enterprise_sizes.rb
@@ -8,12 +8,22 @@ module Import
       graph = RDF::Graph.new
       # concept scheme
       
-      e = EnterpriseSize.new(Vocabulary::TSBDEF["concept/enterprise-size/large"])
-      # e.label
-      #      e.description
-      
-      e.repository.each_statement {|s| graph << s}
-      
+      options = {
+        "large" => "Describes a company with more than 250 employees.",
+        "medium" => "Describes a company with between 51 and 250 employees.",
+        "small" => "Describes a company with between 11 and 50 employees.",
+        "micro" => "Describes a company with up to 10 employees.",
+        "academic" => "Refers to an academic institution where the size of the organization is not specified"
+      }
+      # options are large, medium, small, micro, academic
+      options.each_pair do |label,desc|
+        e = EnterpriseSize.new(Vocabulary::TSBDEF["concept/enterprise-size/#{label}"])
+        e.label = label
+        e.definition = desc
+        e.repository.each_statement {|s| graph << s}
+        
+      end
+
       # write graph to file
       File.open(output_file,'w') {|f| f << graph.dump(:ntriples)}
       

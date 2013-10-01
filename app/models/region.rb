0b77eb2b47142c121a45ddb3fb3ad2c4497a9821
RicSwirrl <ric@swirrl.com>
2013-10-01 21:28:58 +0100

    put regions in their own graph

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,6 +1,7 @@
 class Region
   include TsbResource
-  
+
   rdf_type RDF::URI.new('http://statistics.data.gov.uk/def/statistical-geography')
-  
+  graph_uri "http://#{PublishMyData.local_domain}/graph/regions"
+
 end

fcc083bb09fac4efd3e9ca705c17f375ebb7c981
RicSwirrl <ric@swirrl.com>
2013-10-02 22:42:43 +0100

    basic search

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,11 +2,8 @@ class ProjectsController < ApplicationController
 
   # list of projects
   def index
-
-    # TODO: change to use faceted search.
-    criteria = Project.all
-    @pagination_params = PublishMyData::ResourcePaginationParams.from_request(request)
-    @projects = PublishMyData::Paginator.new(criteria, @pagination_params).paginate
+    @search = Search.new( params )
+    @projects = @search.results
   end
 
   def raw_search

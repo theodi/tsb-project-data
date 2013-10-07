b3798d6c425cd0be93d0d8b8bf5185467340862d
Ed <ed@edforshaw.co.uk>
2013-10-07 14:42:00 +0100

    Navbar arrows

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < PublishMyData::ApplicationController
   helper PublishMyData::Engine.helpers
   helper :all
+
+  before_filter :set_current_nav_tab
+
+  private
+
+  def set_current_nav_tab
+    @current_nav_tab = 'browse'
+  end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,9 @@
 class HomeController < ApplicationController
 
+  private
+
+  def set_current_nav_tab
+    @current_nav_tab = 'about'
+  end
+
 end

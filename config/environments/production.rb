b8a0c9b8ea49c96fc344073b60034f3b50f9c628
RicSwirrl <ric@swirrl.com>
2013-10-10 09:43:39 +0100

    asset compilation fallback (for now)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ TsbProjectData::Application.configure do
   config.assets.js_compressor = :yui
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true # TODO - change this??
 
   # Generate digests for assets URLs
   config.assets.digest = true

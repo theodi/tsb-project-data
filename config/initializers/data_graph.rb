5f3536d9eef3a26dbbda0f0feb63a8103ddbc916
Bill <bill@swirrl.com>
2013-10-16 12:35:00 +0100

    ontology loader and Companies House SIC code retriever

--- config/initializers/data_graph.rb
+++ config/initializers/data_graph.rb
@@ -2,4 +2,18 @@
     DATASET_SLUG = 'tsb-projects-data'
     DATA_GRAPH = "http://#{PublishMyData.local_domain}/graph/#{TsbProjectData::DATASET_SLUG}"
     ONTOLOGY_GRAPH = "http://#{PublishMyData.local_domain}/graph/ontology"
+    ORG_ONTOLOGY_GRAPH = "http://#{PublishMyData.local_domain}/graph/ontology/org"
+    DCTERMS_ONTOLOGY_GRAPH = "http://#{PublishMyData.local_domain}/graph/ontology/dcterms"
+    VCARD_ONTOLOGY_GRAPH = "http://#{PublishMyData.local_domain}/graph/ontology/vcard"
+    FOAF_ONTOLOGY_GRAPH = "http://#{PublishMyData.local_domain}/graph/ontology/foaf"
+    POSTCODE_ONTOLOGY_GRAPH = "http://#{PublishMyData.local_domain}/graph/ontology/postcode"
+    GEO_ONTOLOGY_GRAPH = "http://#{PublishMyData.local_domain}/graph/ontology/geo"
+    ADMINGEO_ONTOLOGY_GRAPH = "http://#{PublishMyData.local_domain}/graph/ontology/admingeo"
+    RDF_ONTOLOGY_GRAPH = "http://#{PublishMyData.local_domain}/graph/ontology/rdf"
+    RDFS_ONTOLOGY_GRAPH = "http://#{PublishMyData.local_domain}/graph/ontology/rdfs"
+    OWL_ONTOLOGY_GRAPH = "http://#{PublishMyData.local_domain}/graph/ontology/owl"
+    SKOS_ONTOLOGY_GRAPH = "http://#{PublishMyData.local_domain}/graph/ontology/skos"
+    TIMELINE_ONTOLOGY_GRAPH = "http://#{PublishMyData.local_domain}/graph/ontology/timeline"
+      
+    
   end

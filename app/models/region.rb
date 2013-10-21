f3f4e00b25dca170e5c9eeb9a88c1b1d9c8930ca
Ed <ed@edforshaw.co.uk>
2013-10-21 12:22:38 +0100

    Region show page

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,7 +1,7 @@
 class Region
   include TsbResource
 
-  rdf_type RDF::URI.new('http://statistics.data.gov.uk/def/statistical-geography')
+  rdf_type 'http://statistics.data.gov.uk/def/statistical-geography'
   DATASET_SLUG = "regions"
   graph_uri "http://#{PublishMyData.local_domain}/graph/#{Region::DATASET_SLUG}"
 

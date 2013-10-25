9e1856a3bad16baa60f97066d43014db8f8b9032
RicSwirrl <ric@swirrl.com>
2013-10-25 11:58:05 +0100

    static home fallback

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
 
   def index
+    @this_year = DateTime.now.year
+    @this_years_projects = get_total_projects_for_this_year(@this_year)
     render layout: 'home'
   end
 
@@ -10,6 +12,32 @@ class HomeController < ApplicationController
 
   def about
     render layout: 'publish_my_data/application'
+  end
+
+  private
+
+  def get_total_projects_for_this_year(year_no)
+    results = Tripod::SparqlClient::Query.select("
+      PREFIX tsb: <http://tsb-projects.labs.theodi.org/def/>
+      PREFIX ptime: <http://purl.org/NET/c4dm/timeline.owl#>
+      PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+      select (COUNT(*) as ?count)
+      where {
+
+      ?project a tsb:Project .
+      ?project tsb:projectDuration ?projectDuration .
+      ?projectDuration ptime:start ?projectStartDate .
+      ?project tsb:competition ?competition .
+      ?competition tsb:budgetArea ?budgetArea .
+
+      FILTER(?projectStartDate >= '#{year_no}-01-01'^^xsd:date) .
+      FILTER(?projectStartDate <= '#{year_no}-12-31'^^xsd:date) .
+
+      }
+    ")
+
+    results[0]["count"]["value"] rescue 0
+
   end
 
 end

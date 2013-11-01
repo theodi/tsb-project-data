0e77cb898a832a7f340466fe6604b34524833b85
RicSwirrl <ric@swirrl.com>
2013-11-01 09:53:39 +0000

    a couple of view fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,24 @@ module ApplicationHelper
   end
 
   def project_participants_from_search_result(project)
-    (0...project.participant_uris.length).collect { |i| link_to project.participant_labels[i], resource_path_from_uri(project.participant_uris[i]) }.join(', ').html_safe
+
+    participant_uris = project.participant_uris
+    participant_labels = project.participant_labels
+
+    leader_index = project.participant_uris.index(project.leader_uri)
+
+    participant_uris.delete_at(leader_index)
+    participant_uris.insert(0, project.leader_uri)
+
+    participant_labels.delete_at(leader_index)
+    participant_labels.insert(0, project.leader_label)
+
+    (0...participant_uris.length).collect do |i|
+      link_to participant_labels[i], resource_path_from_uri(participant_uris[i])
+    end.join(', ').html_safe
   end
+
+
 
   def csv_params(host=nil)
     p = remove_paging_params(params)
